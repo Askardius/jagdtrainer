2eb88a5d12de782910b9b20de595b39e42020517
Askardius <askardius@gmx.de>
2017-10-15 21:25:24 +0200

    First Layout

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<!-- additional scripts and librarys --> 
+<script language="javascript" type="text/javascript" src="lib/jquery-3.2.1.min.js"></script>
+<script language="javascript" type="text/javascript" src="js/jagdtrainer.js"></script>
+<link rel="stylesheet" href="css/jagdtrainer.css">
+</head>
+<body id="body">
+<?php include "modules/module1.php"; ?>
+</body>
+</html>
---
+++ modules/module1.php
@@ -0,0 +1,16 @@
+<!-- This module shows the user different pictures and three possible solitions -->
+<!-- the user then has to chose the fitting answer -->
+<div id="m1mainFrame" class="mainFrame">
+    <div id="m1pictureFrame" class="frameVisible">
+        <div id="m1picture">
+            
+        </div>
+    </div>
+    <div id="m1answerFrame" class="frameVisible">
+        <div id="m1ButtonBox">
+            <button type="button" id="m1button1" class="answerButton">Erdferkel</button>
+            <button type="button" id="m1button2" class="answerButton">Wildschwein</button>
+            <button type="button" id="m1button3" class="answerButton">Osterhase</button>
+        </div>
+    </div>
+</div>
